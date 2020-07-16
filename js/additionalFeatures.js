89e4d1390818b718e49599cf4ef8a74ecacca459
AymarN <aymar.nahum@jecorde.com>
2020-07-16 04:23:04 +0200

    PlayerSelection using div Elements

--- js/additionalFeatures.js
+++ js/additionalFeatures.js
@@ -1,26 +1,34 @@
- $(document).ready(function(){
-    document.getElementById('LoseGame').style.display = 'none';
-    document.getElementById('WinGame').style.display = 'none';
+$(document).ready(function(){
+	document.getElementById('LoseGame').style.display = 'none';
+	document.getElementById('WinGame').style.display = 'none';
        <!-- $("#Start").button().click(function(){ -->
             <!-- player.reset(); -->
             <!-- })  -->
         //$("#dialog .WinGame").dialog();
-        $("#WinGame .my-titlebar-close").click(function(){
+		  
+        $("#LoseGame .my-titlebar-close").click(function(){
             $(this).parent().remove(); 
-        })
-
+        })		
+  
         $("#LoseGame .my-titlebar-close").click(function(){
             $(this).parent().remove(); 
         })
         
         $("#New_game").button().click(function(){
-        player.reset();
+		//if (document.readyState == "complete"){
+            if (document.getElementById("getImagePlayerSelected").innerHTML == "" || document.getElementById("getImagePlayerSelected").innerHTML == undefined){
+                console.log("Select a player");
+			}	
+		
+		//}		
+       
         // Now instantiate your objects.
-        if (document.readyState == "complete"){
+        else if (document.readyState == "complete"){
             if (allEnemies != null || allEnemies != undefined){
                 allEnemies.splice(0,allEnemies.length);
-        }
-        
+			}
+		
+        player.reset();
         var enemy = new Enemy([6.5,120],[100,140]);
         var enemy2 = new Enemy([10,50],[140,180]);
         var enemy3 = new Enemy([20,200],[90,120]);
